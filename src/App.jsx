343ac99807e0709a6be5ce7996b2fe5e9b272811
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-09-19 23:42:06 +0530

    Implement Sidebar, Canvas along with currentMode and nodes state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
+import Sidebar from './components/Sidebar';
+import Canvas from './components/Canvas';
+
 function App() {
+  const currentModeState = useState("add");
+  const nodesState = useState([]);
 
   return (
     <>
+      <div className="mainContainer">
+        <Sidebar currentModeState={currentModeState} />
+        <Canvas currentModeState={currentModeState} nodesState={nodesState} />
+      </div>
     </>
   )
 }
