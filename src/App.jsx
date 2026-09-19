eca14ad36168f20b0beacc997ad3220ff706a4c3
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-09-20 01:39:24 +0530

    Create selectedNodeState and pass it to Sidebar and Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import Canvas from './components/Canvas';
 function App() {
   const currentModeState = useState("add");
   const nodesState = useState([]);
+  const selectedNodeState = useState();
 
   return (
     <>
       <div className="mainContainer">
-        <Sidebar currentModeState={currentModeState} />
-        <Canvas currentModeState={currentModeState} nodesState={nodesState} />
+        <Sidebar currentModeState={currentModeState} selectedNodeState={selectedNodeState} />
+        <Canvas currentModeState={currentModeState} nodesState={nodesState} selectedNodeState={selectedNodeState} />
       </div>
     </>
   )
